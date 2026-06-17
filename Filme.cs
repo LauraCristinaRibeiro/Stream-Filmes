04923c986607c7a5bef61900d983af2c8f7f0f3c
Laura Ribeiro <lauracristinaribeiro@hotmail.com>
2026-06-17 19:51:05 -0300

    feat: cria classe Filme

---
+++ Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+
+public class Filme
+{
+    public string titulo;
+    public string genero;
+    public int anoLancamento;
+    public int classificacaoIndicativa;
+
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine("\n=== Filme Cadastrado ===");
+        Console.WriteLine("Título: " + titulo);
+        Console.WriteLine("Gênero: " + genero);
+        Console.WriteLine("Ano de lançamento: " +anoLancamento);
+        Console.WriteLine("Classificação: " + classificacaoIndicativa);
+    }
+}

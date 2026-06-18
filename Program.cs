6346e1db9991bf93c9d8424a5ae5bc46468cb472
Laura Ribeiro <lauracristinaribeiro@hotmail.com>
2026-06-17 21:32:36 -0300

    feat: implementa entrada de dados do usuário

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿Filme filme = new Filme();
-filme.titulo = "Avatar";
-filme.genero = "Ficção Científica";
-filme.anoLancamento = 2009;
-filme.classificacaoIndicativa = 12;
+
+Console.WriteLine("Título: ");
+filme.titulo = Console.ReadLine();
+
+Console.WriteLine("Gênero: ");
+filme.genero = Console.ReadLine();
+
+Console.WriteLine("Ano de lançamento: ");
+filme.anoLancamento = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Classificação indicativa: ");
+filme.classificacaoIndicativa = Convert.ToInt32(Console.ReadLine());
 
 filme.ExibirInformacoes();

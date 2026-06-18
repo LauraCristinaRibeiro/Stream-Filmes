2fa2cc5f5515776f5581475516a1a3e09305d764
Laura Ribeiro <lauracristinaribeiro@hotmail.com>
2026-06-18 19:53:56 -0300

    feat: adiciona regra classificação

--- Filme.cs
+++ Filme.cs
@@ -7,12 +7,21 @@ public class Filme
     public int anoLancamento;
     public int classificacaoIndicativa;
 
+    public string ObterClassificacao()
+    {
+        if (classificacaoIndicativa <= 0)
+        {
+            return "Livre";
+        }
+        return classificacaoIndicativa + " anos";
+    }
+
     public void ExibirInformacoes()
     {
         Console.WriteLine("\n=== Filme Cadastrado ===");
         Console.WriteLine("Título: " + titulo);
         Console.WriteLine("Gênero: " + genero);
         Console.WriteLine("Ano de lançamento: " +anoLancamento);
-        Console.WriteLine("Classificação: " + classificacaoIndicativa);
+        Console.WriteLine("Classificação: " + ObterClassificacao());
     }
 }

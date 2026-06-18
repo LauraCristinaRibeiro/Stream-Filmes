c75e2d45f82b3827cda74b0a3662829bc05be199
Laura Ribeiro <lauracristinaribeiro@hotmail.com>
2026-06-18 19:41:11 -0300

    refatora código

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿Filme filme = new Filme();
 
-<<<<<<< HEAD
 Console.Write("Título: ");
 filme.titulo = Console.ReadLine();
 
@@ -11,18 +10,7 @@ Console.Write("Ano de lançamento: ");
 filme.anoLancamento = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Classificação indicativa: ");
-=======
-Console.WriteLine("Título: ");
-filme.titulo = Console.ReadLine();
 
-Console.WriteLine("Gênero: ");
-filme.genero = Console.ReadLine();
-
-Console.WriteLine("Ano de lançamento: ");
-filme.anoLancamento = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Classificação indicativa: ");
->>>>>>> feat/cadastro-filme
 filme.classificacaoIndicativa = Convert.ToInt32(Console.ReadLine());
 
 filme.ExibirInformacoes();
